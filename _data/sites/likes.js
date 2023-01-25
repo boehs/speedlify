418afff2b5bce38e153a04882ecd292d3d4ae681
Evan Boehs <evan@boehs.org>
2023-01-24 23:23:15 -0500

    reenable freq

--- _data/sites/likes.js
+++ _data/sites/likes.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Sites I like", // optional, falls back to object key
 	description: ":)",
 	options: {
-		//frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 23, // (in minutes), 23 hours
 		noShame: true
 	},
 	urls: [
