50170979484a1ba43012e8762bc09415d85a0664
Evan Boehs <evan@boehs.org>
2023-01-24 20:57:32 -0500

    customize

---
+++ _data/sites/likes.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Sites I like", // optional, falls back to object key
+	description: ":)",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://boehs.org",
+		"https://solar.lowtechmagazine.com",
+		"https://www.stevegattuso.me",
+		"https://cjlm.ca",
+		"https://100r.co"
+	]
+};
